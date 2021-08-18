9cc797f3e008a6b4d0a73acdb7628e271ae72887
Nikonov Vladzimir <nikonovbox@gmail.com>
2021-08-18 20:31:56 +0300

    start commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1111;
+        int milesPrice = 20;
+        int totalBonus = price / milesPrice;
+        System.out.println("Начислено миль:" + totalBonus);
+    }
+}
